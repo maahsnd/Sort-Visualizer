a6d29c06207fcb25076150d0dbaaf757525ee1da
mrwindupbirdesq <nmaahs11@gmail.com>
2024-01-02 14:27:53 -0800

    Display array

--- src/components/sort_visualizer/SortVisualizer.jsx
+++ src/components/sort_visualizer/SortVisualizer.jsx
@@ -1,14 +1,26 @@
-import styles from './sortVisualizer.module.css'
-import SortDash from '../sort_dashboard/SortDash'
+import { useState } from 'react';
+import uniqid from 'uniqid';
+import styles from './sortVisualizer.module.css';
+import SortDash from '../sort_dashboard/SortDash';
+import randomizeArray from '../../randomizeArray';
 
 function SortVisualizer() {
+  const [array, setArray] = useState(randomizeArray());
 
-    return (
-      <div className={styles.container}>
-        <SortDash></SortDash>
+  return (
+    <div className={styles.container}>
+      <div className={styles.arrayContainer}>
+        {array.map((el) => (
+          <div
+          style={{height: (el / 10) +'%'}}
+            className={`${styles.intBar} `}
+            key={uniqid()}
+          ></div>
+        ))}
       </div>
-    )
-  }
-  
-  export default SortVisualizer
-  
+      <SortDash></SortDash>
+    </div>
+  );
+}
+
+export default SortVisualizer;
