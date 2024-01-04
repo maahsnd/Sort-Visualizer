6430ac99e64d41024c2987578009cd25c69dac70
mrwindupbirdesq <nmaahs11@gmail.com>
2024-01-03 18:34:02 -0800

    Add merge on click

--- src/components/sort_dashboard/SortDash.jsx
+++ src/components/sort_dashboard/SortDash.jsx
@@ -1,14 +1,13 @@
 import styles from './sortDash.module.css'
 
-function SortDash({newArr}) {
+function SortDash({newArr, mergeSort}) {
 
     return (
       <div className={styles.dashContainer}>
         <button className={styles.newArray} onClick={()=>newArr()}> Generate New Array</button>
-        <button className={styles.mergeSort}> Merge Sort</button>
+        <button className={styles.mergeSort} onClick={()=>mergeSort()}> Merge Sort</button>
       </div>
     )
   }
   
   export default SortDash
-  
